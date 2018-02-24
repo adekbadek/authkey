63ad526b8a1f15aef13b34faca4d141c548935f3
Adam Borowski <adam@adamboro.com>
2018-02-24 22:18:49 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const { generate } = require('./keys')
+const { validateEmail } = require('./helpers')
+
+const app = express()
+
+const PORT = 3000
+
+app.post('/request-key/:address', (req, res) => {
+  const {address} = req.params
+  if (validateEmail(address)) {
+    res.send({message: `done, key is ${generate}`})
+  } else {
+    res.status(400).send({error: `${address} is not a valid email address`})
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
